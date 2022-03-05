36cdb0f9d04f63c92c218135d339a42aa85370bf
Brian Esquivel <brian.esquive@gmail.com>
2022-03-04 19:12:22 -0600

    Práctica #4

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -9,4 +9,31 @@ export class Tab1Page {
 
   constructor() {}
 
+  alumnos = [
+    {
+      "nombre": "Brian",
+      "apellido": "Esquivel",
+      "matricula": "1930115"
+    },
+    {
+      "nombre": "Omar",
+      "apellido": "Garza",
+      "matricula": "1928432"
+    },
+    {
+      "nombre": "Sergio",
+      "apellido": "Gutiérrez",
+      "matricula": "1929456"
+    },
+    {
+      "nombre": "Diego",
+      "apellido": "Jasso",
+      "matricula": "1930103"
+    },
+    {
+      "nombre": "Ángel",
+      "apellido": "Rodríguez",
+      "matricula": "1930238"
+    }
+  ];
 }
