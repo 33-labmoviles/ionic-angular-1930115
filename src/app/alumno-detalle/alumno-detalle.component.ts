c4e6cdaa57c0f53b177dd3d43d1e66f97bdecead
Brian Esquivel <brian.esquive@gmail.com>
2022-03-29 15:17:08 -0600

    Práctica #6 Finalizada

--- src/app/alumno-detalle/alumno-detalle.component.ts
+++ src/app/alumno-detalle/alumno-detalle.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-alumno-detalle',
@@ -8,7 +9,9 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class AlumnoDetalleComponent implements OnInit {
 
-  constructor(private ruta: ActivatedRoute) { }
+  constructor(
+    private ruta: ActivatedRoute, 
+    public actionSheetC: ActionSheetController) { }
 
   ngOnInit() {
     this.obtenerDetalleAlumno(this.matricula);
@@ -59,6 +62,49 @@ export class AlumnoDetalleComponent implements OnInit {
     }
 
     return this.alumnoDetalle;
+  }
+
+  async mostrarActionSheet(){
+    const actionSheet = await this.actionSheetC.create({
+      header: 'Opciones',
+      cssClass: 'mi-primer-action-sheet',
+      buttons: [{
+        text: 'Delete',
+        role: 'destructive',
+        icon: 'trash',
+        id: 'delete-button',
+        data: {
+          type: 'delete'
+        },
+        handler: () => {
+          console.log('Delete clicked');
+        }
+      }, {
+        text: 'Share',
+        icon: 'share',
+        data: 10,
+        handler: () => {
+          console.log('Share clicked');
+        }
+      }, {
+        text: 'Favorite',
+        icon: 'heart',
+        handler: () => {
+          console.log('Favorite clicked');
+        }
+      }, {
+        text: 'Cancel',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+          console.log('Cancel clicked');
+        }
+      }]
+    });
+    await actionSheet.present();
+
+    const { role, data } = await actionSheet.onDidDismiss();
+    console.log('onDidDismiss resolved with role and data', role, data);
   }
 
 }
